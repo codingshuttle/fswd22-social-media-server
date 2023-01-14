c5e60ed4dbba9c55c8ee77bf97435ea6cc75296b
Anuj Kumar Sharma <anuj55149@gmail.com>
2023-01-14 16:46:19 +0530

    updating route to HashRouter

--- index.js
+++ index.js
@@ -5,33 +5,36 @@ const authRouter = require("./routers/authRouter");
 const postsRouter = require("./routers/postsRouter");
 const userRouter = require("./routers/userRouter");
 const morgan = require("morgan");
-const cookieParser = require('cookie-parser');
-const cors = require('cors')
-const cloudinary = require('cloudinary').v2;
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const cloudinary = require("cloudinary").v2;
 
 dotenv.config("./.env");
 
-// Configuration 
+// Configuration
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+    secure: true,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
 
 //middlewares
-app.use(express.json({limit: '10mb'}));
+app.use(express.json({ limit: "10mb" }));
 app.use(morgan("common"));
-app.use(cookieParser())
-app.use(cors({
-    credentials: true,
-    origin: process.env.CLIENT_ORIGIN
-}))
+app.use(cookieParser());
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.CLIENT_ORIGIN,
+    })
+);
 
 app.use("/auth", authRouter);
 app.use("/posts", postsRouter);
-app.use('/user', userRouter)
+app.use("/user", userRouter);
 app.get("/", (req, res) => {
     res.status(200).send("OK from Server");
 });
