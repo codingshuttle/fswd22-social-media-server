88450efb4011614f031e70c49ac541dc83ce31be
Anuj Kumar Sharma <anuj55149@gmail.com>
2023-01-14 21:58:39 +0530

    prod and dev scripts added

--- index.js
+++ index.js
@@ -25,15 +25,16 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(morgan("common"));
 app.use(cookieParser());
-// let origin = 'http://localhost:3000';
-// console.log('here env', process.env.NODE_ENV);
-// if(process.env.NODE_ENV === 'production') {
-//     origin = process.env.CLIENT_ORIGIN;
-// }
+
+let origin = 'http://localhost:3000';
+console.log('here env', process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'production') {
+    origin = process.env.CORS_ORIGIN;
+}
 app.use(
     cors({
         credentials: true,
-        origin: process.env.CORS_ORIGIN
+        origin
     })
 );
 
