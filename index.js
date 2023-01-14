8bdc54ddc761ffe2d0b0a20892f435f09c571d0e
Anuj Kumar Sharma <anuj55149@gmail.com>
2023-01-14 21:39:50 +0530

    cors origin updated to client url

--- index.js
+++ index.js
@@ -25,15 +25,15 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(morgan("common"));
 app.use(cookieParser());
-let origin = 'http://localhost:3000';
-console.log('here env', process.env.NODE_ENV);
-if(process.env.NODE_ENV === 'production') {
-    origin = process.env.CLIENT_ORIGIN;
-}
+// let origin = 'http://localhost:3000';
+// console.log('here env', process.env.NODE_ENV);
+// if(process.env.NODE_ENV === 'production') {
+//     origin = process.env.CLIENT_ORIGIN;
+// }
 app.use(
     cors({
         credentials: true,
-        origin
+        origin: process.env.CORS_ORIGIN
     })
 );
 
