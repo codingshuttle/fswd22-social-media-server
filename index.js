ede272956cda98730fef6f2dfaa48d058861393c
Anuj Kumar Sharma <anuj55149@gmail.com>
2023-01-14 16:30:09 +0530

    client origin added

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(morgan("common"));
 app.use(cookieParser())
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000'
+    origin: process.env.CLIENT_ORIGIN
 }))
 
 app.use("/auth", authRouter);
